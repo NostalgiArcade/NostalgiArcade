b6eb7e7ca6910c9d4408a0f9278d8ac5a2fdd76b
Yuri Zanini <108382693+yurizanini@users.noreply.github.com>
2024-03-11 12:36:07 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-require('dotenv').config();
-
-const username = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
